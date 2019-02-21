82f9f701621797f7c2c6423c277c58c6e33058bd
Lorena377 <alamo_roman@hotmail.com>
2019-02-21 15:25:29 -0500

    estructura html y botones

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-/* Acá va tu código */
+const cifrar = document.getElementById("cifrar");
+function CIFRAR(){
+ 
+}
+cifrar.addEventListener('click',CIFRAR);
+
+const decifrar = document.getElementById("decifrar");
+
+function decifrar (){ 
+  
+}
+decifrar.addEventListener('click', decifrar);
+
+const texto= ANA
+const desplazamiento= 3
+
+for (let i=0; i<= palabra.length;i++)
+  {
+    resultado=String.fromCharCode((palabra.charCodeAt(i)-65+desplazamiento)%26+65)
+document.write(resultado)
+  }
