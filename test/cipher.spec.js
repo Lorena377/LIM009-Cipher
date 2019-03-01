8b62e90bf414a950943b29ca04031c185935eeea
Lorena377 <alamo_roman@hotmail.com>
2019-03-01 10:56:36 -0500

    testeando con lulu en el branch test

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,1 +1,13 @@
-
+describe("cipher",()=>{
+  it("cipher deberia ser un objeto",()=>{
+    assert.equal(typeof cipher,"object");
+  });
+describe("cipher.suma",()=>{
+  it("cipher.suma deberia ser una function", ()=>{
+    assert.equal(typeof cipher.suma,"function");
+  })
+  it("cipher.suma debe sumar dos numeros para ello cuando a=3 y b=5, debe retornar 8",()=>{
+    assert.equal(cipher.suma(3,5),8)
+  })
+})
+});
